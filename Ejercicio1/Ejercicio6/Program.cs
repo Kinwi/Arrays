8e5ef4ff2f9b1cd9d03b94eab429bb454d41c427
Raúl López Hernando <raullopezhernando@gmail.com>
2019-10-01 18:55:53 +0200

    Hasta el ejercicio 7

--- Ejercicio1/Ejercicio6/Program.cs
+++ Ejercicio1/Ejercicio6/Program.cs
@@ -6,7 +6,63 @@ namespace Ejercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            /*
+             6.Poner los nombres de los meses en un array de 12. 
+             Cuando el usuario introduce un número del 1 al 12, 
+             mostrar el nombre de ese mes en pantalla.
+
+          */
+
+            string[] meses= { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre","Noviembre","Diciembre" };
+            Console.WriteLine("Escribe un numero del 1 al 12");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            switch (numero)
+
+            {
+                case 1:
+                    Console.WriteLine(meses[0]);
+                    break;
+                case 2:
+                    Console.WriteLine(meses[1]);
+                    break;
+                case 3:
+                    Console.WriteLine(meses[2]);
+                    break;
+
+                case 4:
+                    Console.WriteLine(meses[3]);
+                    break;
+                case 5:
+                    Console.WriteLine(meses[4]);
+                    break;
+                case 6:
+                    Console.WriteLine(meses[5]);
+                    break;
+                case 7:
+                    Console.WriteLine(meses[6]);
+                    break;
+                case 8:
+                    Console.WriteLine(meses[7]);
+                    break;
+                case 9:
+                    Console.WriteLine(meses[8]);
+                    break;
+
+                case 10:
+                    Console.WriteLine(meses[9]);
+                    break;
+                case 11:
+                    Console.WriteLine(meses[10]);
+                    break;
+                case 12:
+                    Console.WriteLine(meses[11]);
+                    break;
+
+
+                default:
+                    Console.WriteLine("El valor introducido no es correcto");
+                    break;
+         }  }
     }
 }
---
+++ Ejercicio1/Ejercicio7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7. Hacer la media de 10 valores utilizando un array.
+
+            Console.WriteLine("Introduce 10 valores");
+
+            int sumaArrays = 0;
+
+            int[] arrayNumeros = new int[10];
+
+            for (int i = 0; i < arrayNumeros.Length; i++)
+            {
+                int numeros = Convert.ToInt32(Console.ReadLine());
+
+                arrayNumeros[i] = numeros;
+
+                sumaArrays = sumaArrays + arrayNumeros[i];
+
+            }
+
+            Console.WriteLine("La media de todos los numeros introducidos en el Array es : " + sumaArrays / 2);
+            
+
+        }
+    }
+}
